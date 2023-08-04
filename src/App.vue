a638618254b1741f9092e31356bda415b37a538e
hyeonho64 <hyeonhokang10@kakao.com>
2023-08-04 11:08:20 +0900

    Version 1.2.2

--- src/App.vue
+++ src/App.vue
@@ -6,6 +6,6 @@
 @import "./assets/font.css";
 
 body, html {
-  background: linear-gradient(to right, #C2FF021A, #AA32911A);
+  background: linear-gradient(to right, #C2FF021A, #AA32910D);
 }
 </style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,17 +3,17 @@
   <div style="margin-left: 3rem; margin-right: 3rem;">
   <div class="container-fluid pb-3">
     <div class="d-grid gap-3" style="grid-template-columns: 1fr 1fr; height: 85vh;">
-      <div class="bg-body-tertiary border rounded-3">
+      <div class="border rounded-3">
       </div>
-      <div class="bg-body-tertiary border rounded-3">
+      <div class="border rounded-3">
       </div>
     </div>
   </div>
   <div class="container-fluid pb-3">
     <div class="d-grid gap-3" style="grid-template-columns: 1fr 1fr; height: 85vh;">
-      <div class="bg-body-tertiary border rounded-3">
+      <div class="border rounded-3">
       </div>
-      <div class="bg-body-tertiary border rounded-3">
+      <div class="border rounded-3">
       </div>
     </div>
   </div>
