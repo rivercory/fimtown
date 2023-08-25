feaf047bb0528002c8ac45a97480db1e07e67a3a
hyeonho64 <hyeonhokang10@kakao.com>
2023-08-26 08:49:09 +0900

    Version 1.6.2

---
+++ src/lib/supabaseClient.js
@@ -0,0 +1,3 @@
+import { createClient } from '@supabase/supabase-js'
+
+export const supabase = createClient('https://chvxhvyrbalhmzrqymee.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNodnhodnlyYmFsaG16cnF5bWVlIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTE4OTM0NjUsImV4cCI6MjAwNzQ2OTQ2NX0.iYKEpE2n3Zmut1HPD7DKzedDvArWTf-h_Qyyqc2NSTY')
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -3,11 +3,26 @@
   <div class="about rounded-3 border">
     <h4 style="font-weight: bold; font-family: LINESeedKR-Bd;">소개</h4>
   </div>
+  <ul>
+    <li v-for="country in countries" :key="country.id">{{ country.name }}</li>
+  </ul>
 </template>
 
 <script setup>
+import { ref, onMounted } from 'vue'
+import { supabase } from '../lib/supabaseClient'
 import NavMenu from "@/components/NavMenu.vue";
-import SideMenu from "@/components/SideMenu.vue";
+
+const countries = ref([])
+
+async function getCountries() {
+  const { data } = await supabase.from('countries').select()
+  countries.value = data
+}
+
+onMounted(() => {
+  getCountries()
+})
 </script>
 
 <style>
