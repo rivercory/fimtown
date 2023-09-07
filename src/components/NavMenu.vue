a75759b88038cbfdbb14c0157214d903d2e9cbe3
hyeonho64 <hyeonhokang10@kakao.com>
2023-09-07 21:28:38 +0900

    Version 1.6.10

--- src/components/NavMenu.vue
+++ src/components/NavMenu.vue
@@ -29,29 +29,12 @@ export default {
       <div class="offcanvas-body">
         <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
           <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="#">Home</a>
+            <router-link class="nav-link" to="/">홈</router-link>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="#">Link</a>
-          </li>
-          <li class="nav-item dropdown">
-            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-              Dropdown
-            </a>
-            <ul class="dropdown-menu">
-              <li><a class="dropdown-item" href="#">Action</a></li>
-              <li><a class="dropdown-item" href="#">Another action</a></li>
-              <li>
-                <hr class="dropdown-divider">
-              </li>
-              <li><a class="dropdown-item" href="#">Something else here</a></li>
-            </ul>
+            <router-link class="nav-link" to="about">소개</router-link>
           </li>
         </ul>
-        <form class="d-flex mt-3" role="search">
-          <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-          <button class="btn btn-outline-success" type="submit">Search</button>
-        </form>
       </div>
     </div>
   </nav>
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,7 +1,7 @@
 <template>
   <NavMenu title="소개"/>
   <div class="about rounded-3 border">
-    <h4 style="font-weight: bold; font-family: LINESeedKR-Bd;">소개</h4>
+    <h4 style="font-weight: bold; font-family: LINESeedKR-Bd;">엔써타운이란?</h4>
   </div>
 </template>
 
@@ -11,3 +11,7 @@
   padding: 1rem;
 }
 </style>
+
+<script setup>
+import NavMenu from "@/components/NavMenu.vue";
+</script>
